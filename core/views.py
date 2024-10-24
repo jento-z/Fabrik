a3f2cd068c86a91ecbe6de30d52ca3ca7bf44d67
melanie-wiegand <mwiegan6@uoregon.edu>
2024-10-24 15:14:48 -0700

    more login/signup stuff

--- core/views.py
+++ core/views.py
@@ -2,14 +2,21 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
-#import models
+from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 
 
 # Create your views here.
+
+@login_required(login_url='signin')
 def index(request):
     #return HttpResponse('<h1>Welcome To Social Book</h1>')
+    
     return render(request, 'index.html')
+
+def settings(request):
+    return render(request, 'setting.html')
 
 def signup(request):
     
@@ -29,14 +36,38 @@ def signup(request):
             else:
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
-                #Log user in and direct to settings page
+                #Log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+
                 #create profile object for new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('login')
+                return redirect('settings')
         else:
             messages.info(request, 'Passwords Not Matching')
             return redirect('signup')
     else:        
         return render(request, 'signup.html')
+    
+def signin(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/")
+        else:
+            messages.info(request, "Credentials Invalid")
+            return redirect('signin')
+    else:
+        return render(request, 'signin.html')
+    
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')

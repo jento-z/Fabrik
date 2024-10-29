18ae90cf285938c1d1fe821cb3c29fbc38e57706
melanie-wiegand <mwiegan6@uoregon.edu>
2024-10-29 14:06:54 -0700

    functioning

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('settings', views.settings, name='settings'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
-    path('logout', views.logout, name = 'logout')
-    path('profile/<str:pk>', views.profile, name='profile'),
+    path('logout', views.logout, name = 'logout'),
+    # path('profile/<str:pk>', views.profile, name='profile'),
     path('signup', views.signup, name='signup')
 ]

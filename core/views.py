2413c706d68edec5f5264d5bf6c11f2c4b20207e
melanie-wiegand <mwiegan6@uoregon.edu>
2024-11-06 14:35:54 -0800

    fixed bio and location updating

--- core/views.py
+++ core/views.py
@@ -23,20 +23,20 @@ def settings(request):
     if request.method == 'POST':
         if request.FILES.get('image') == None:
             image = user_profile.profileimg
-            # bio = request.POST['bio']
+            bio = request.POST['bio']
             location = request.POST['location']
 
             user_profile.profileimg = image
-            # user_profile.bio = bio
+            user_profile.bio = bio
             user_profile.location = location
             user_profile.save()
         if request.FILES.get('image') != None:
             image = request.FILES.get('image')
-            # bio = request.POST['bio']
+            bio = request.POST['bio']
             location = request.POST['location']
 
             user_profile.profileimg = image
-            # user_profile.bio = bio
+            user_profile.bio = bio
             user_profile.location = location
             user_profile.save()
 

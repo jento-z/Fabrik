8950a6c50bf251b46f260e05ba4ef1ca2446bc73
arturod <6arturodiaz@gmail.com>
2024-11-08 15:49:13 -0800

    Added Arturo usr, and added more checks for password

--- core/views.py
+++ core/views.py
@@ -24,6 +24,7 @@ def like_post(request):
     username = request.user.username
     post_id = request.GET.get('post_id')
 
+    # TODO: Need to only display following posts
     post = Post.objects.get(id=post_id)
     
     like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
@@ -76,28 +77,39 @@ def signup(request):
         password = request.POST['password']
         password2 = request.POST['password2']
 
-        if password == password2:
-            if User.objects.filter(email=email).exists():
-                messages.info(request, 'Email Taken')
-                return redirect('signup')
-            elif User.objects.filter(username=username).exists():
-                messages.info(request, 'Username Taken')
-                return redirect('signup')
-            else:
-                user = User.objects.create_user(username=username, email=email, password=password)
-                user.save()
-                #Log user in and redirect to settings page
-                user_login = auth.authenticate(username=username, password=password)
-                auth.login(request, user_login)
-
-                #create profile object for new user
-                user_model = User.objects.get(username=username)
-                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
-                new_profile.save()
-                return redirect('settings')
-        else:
+        # Password guidelines
+        if password != password2:
             messages.info(request, 'Passwords Not Matching')
             return redirect('signup')
+        elif len(password) < 8:
+            messages.info(request, 'Password must contain at least 8 characters')
+            return redirect('signup')
+        elif password.isdigit():
+            messages.info(request, 'Password cannot be entirely numeric')
+            return redirect('signup')
+        
+        # Check if the username or email already exists
+        if User.objects.filter(email=email).exists():
+            messages.info(request, 'Email Taken')
+            return redirect('signup')
+        elif User.objects.filter(username=username).exists():
+            messages.info(request, 'Username Taken')
+            return redirect('signup')
+        else:
+            # Create the user if all checks pass
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save()
+            
+            # Log user in and redirect to settings page
+            user_login = auth.authenticate(username=username, password=password)
+            auth.login(request, user_login)
+
+            # Create profile object for the new user
+            user_model = User.objects.get(username=username)
+            new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+            new_profile.save()
+            
+            return redirect('settings')
     else:        
         return render(request, 'signup.html')
     

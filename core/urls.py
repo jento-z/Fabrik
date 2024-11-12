830727a9470c17efc82f8b96384a5f73a216878b
melanie-wiegand <mwiegan6@uoregon.edu>
2024-11-12 15:36:55 -0800

    search page working

--- core/urls.py
+++ core/urls.py
@@ -5,12 +5,12 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('settings', views.settings, name='settings'),
     path('upload', views.upload, name='upload'),
-    path('signup', views.signup, name='signup'),
+    path('follow', views.follow, name='follow'),
+    path('search', views.search, name='search'),
+    path('profile/<str:pk>', views.profile, name='profile'),
+    path('like-post', views.like_post, name = 'like-post'),
     path('signin', views.signin, name='signin'),
     path('logout', views.logout, name = 'logout'),
-    path('like-post', views.like_post, name = 'like-post'),
-    path('profile/<str:pk>', views.profile, name='profile'),
-    path('follow', views.follow, name='follow'),
     path('signup', views.signup, name='signup'),
     path('create-outfit/', views.create_outfit, name='create-outfit')
 ]

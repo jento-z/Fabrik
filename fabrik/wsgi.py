5ba8bba43d0d6a74592322513247d18eff841d97
Jackson Zenti <130002884+jentille@users.noreply.github.com>
2024-12-05 21:08:30 -0800

    added deployment.py, adjusted wsgi.py to work with azure
    added requirements.txt as well

--- fabrik/wsgi.py
+++ fabrik/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+settings_module = 'azure_project.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'azure_project.settings'
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fabrik.settings')
 
 application = get_wsgi_application()

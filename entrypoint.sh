70e2408fbf243a620c19273c07055a7e1c63be7a
Jackson Zenti <130002884+jentille@users.noreply.github.com>
2024-12-06 16:31:45 -0800

    adjusted entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 echo "Running collectstatic..."
 python manage.py collectstatic --noinput
 
+echo "Running gunicorn..."
+gunicorn fabrik.wsgi:application --bind 0.0.0.0:8000
+
 echo "Starting server..."
 exec "$@"

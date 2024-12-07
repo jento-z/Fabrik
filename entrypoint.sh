e93b0e1f4d8492de41c882cf63c1ae21d010cee5
Jackson Zenti <130002884+jentille@users.noreply.github.com>
2024-12-06 16:38:05 -0800

    removed port assignment in guinicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo "Running collectstatic..."
 python manage.py collectstatic --noinput
 
 echo "Running gunicorn..."
-gunicorn fabrik.wsgi:application --bind 0.0.0.0:8000
+gunicorn fabrik.wsgi
 
 echo "Starting server..."
 exec "$@"

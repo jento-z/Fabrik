2b8c89e1e33e7b780efa1ec58c2c1c3cf23b1181
Jackson Zenti <130002884+jentille@users.noreply.github.com>
2024-12-06 18:59:24 -0800

    migrated sqlite to postgres on render

--- fabrik/settings.py
+++ fabrik/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,6 +85,8 @@ DATABASES = {
     }
 }
 
+database_url = os.environ.get("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
